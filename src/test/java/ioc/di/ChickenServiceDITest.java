5c8c5f6d7e6c1b3fd61a32a0ca657b10b1ad8c8f
NamYoung Kim <095201a@naver.com>
2022-12-20 09:10:31 +0900

    feat : 테스트 코드 가독성 개선

--- src/test/java/ioc/di/ChickenServiceDITest.java
+++ src/test/java/ioc/di/ChickenServiceDITest.java
@@ -4,12 +4,14 @@ import ioc.di.domain.Chicken;
 import ioc.di.domain.ChickenGender;
 import ioc.di.ioc.IOCContainer;
 import ioc.di.service.ChickenService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ChickenServiceDITest {
 
+    @DisplayName("Service 클래스의 Repository 필드 주입 여부를 확인한다.")
     @Test
      void diTest() {
 
